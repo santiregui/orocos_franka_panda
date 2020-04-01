50bed672bd05fb5bd2bd24e97c324590320e9c6c
Santiago Iregui <santiago.iregui@kuleuven.be>
2020-04-01 19:36:37 +0200

    Currently working for operation that obtains current angles

--- src/franka_panda_driver-component.cpp
+++ src/franka_panda_driver-component.cpp
@@ -14,54 +14,37 @@ void setDefaultBehavior(FrankaComponent::PandaPtr& panda) {
   panda->setCartesianImpedance({{3000, 3000, 3000, 300, 300, 300}});
 }
 
-FrankaComponent::FrankaComponent(std::string const& name) 
-  : TaskContext(name)
-  , m_q_actual(NR_JOINT)
-  , m_q_des(NR_JOINT)
-  , m_t_actual(NR_JOINT)
-  , m_qdot_actual(NR_JOINT)
-  , p_baseframe("base_link")
+FrankaComponent::FrankaComponent(std::string const& name): TaskContext(name,PreOperational)
   , p_ip_address("172.16.0.2")
+  , sending_setpoints(false)
   {
-    
+
   /* Setting up Orocos component interface */
   //Adding ports
   /// Input
-  this->addPort("event_in",              port_ein).doc("Events IN - eg supervisor");
-  this->addPort("q_desired",             port_qdes).doc("desired joint position [rad]");
-  this->addPort("JointPositionCommand",  port_joint_pos_command).doc("desired joint positions [rad]");
-  this->addPort("JointVelocityCommand",  port_joint_vel_command).doc("desired joint velocities [rad/s]");
+  this->addPort("control_joint_velocities",  control_joint_velocities).doc("Desired joint velocities [rad/s]");
   /// Output
-  this->addPort("event_out",             port_eout).doc("Events OUT - eg faults to supervisor");
-  this->addPort("q_actual",              port_q_actual).doc("current joint positions [rad]");
-  this->addPort("qdot_actual",           port_qdot_actual).doc("current joint velocities [rad/s]");
-  this->addPort("JointPositionMeasured", port_joint_pos_msr).doc("current joint positions [rad]");
-  this->addPort("JointExternalEffort",   port_joint_ext_jnt).doc("external torque on joints [Nm]");
-  this->addPort("joint_states",          port_joint_state).doc("joint_states (ROS)");
+  this->addPort("sensor_joint_angles",             sensor_joint_angles).doc("Current sensed joint angles [rad]");
+  this->addPort("tool_external_wrench",          tool_external_wrench).doc("External wrench calculated from the measured data: three forces [N] and three moments [Nm]");
   // Properties
-  this->addProperty("baseframe",  p_baseframe).doc("Frame name of the robot base");
-  this->addProperty("ip_address", p_ip_address).doc("Frame name of the robot base");
-  
-  //
-  m_t_ext.names.resize(NR_JOINT);
-  m_t_ext.efforts.assign(NR_JOINT,0);
+  this->addProperty("ip_address", p_ip_address).doc("ip_address of the robot's controller. Default: 172.16.0.2");
 
-  m_joint_states.name.resize(NR_JOINT);
-  m_joint_states.position.resize(NR_JOINT);
-  m_joint_states.effort.resize(NR_JOINT);
-  m_joint_states.header.frame_id = p_baseframe;
+  //Operations
+  this->addOperation("start_sending_setpoints",  &FrankaComponent::start_sending_setpoints, this, OwnThread).doc("Starts sending the setpoints of the joints to the robot");
+  this->addOperation("get_joint_angles",  &FrankaComponent::get_joint_angles, this, OwnThread).doc("Get the current sensed joint angles [rad]. Useful to initialize your trajectory planner.");
 
-  for (unsigned int i=0; i<NR_JOINT; ++i) {
-    std::ostringstream ss;
-    ss << "arm_" << i+1 << "_joint";
-    m_joint_states.name[i] = ss.str();
-    m_t_ext.names[i]       = ss.str();
-  }
-  port_joint_state.setDataSample(m_joint_states);
+  //Memory allocation for the size of the vectors done beforehand (real time)
+  temporary_desired_vel.resize(NR_JOINT,0.0);
+  temporary_actual_pos.resize(NR_JOINT,0.0);
+  temporary_actual_wrench.resize(6,0.0);
 
-  
+  sensor_joint_angles.setDataSample( temporary_actual_pos );
+  tool_external_wrench.setDataSample( temporary_actual_wrench );
+
+
+
   try {
-    panda = std::make_unique<franka::Robot>(franka::Robot("192.168.0.1"));
+    panda = std::make_unique<franka::Robot>(franka::Robot(p_ip_address));
   } catch (const franka::Exception& e) {
     std::cout << e.what() << std::endl;
   }
@@ -79,14 +62,19 @@ bool FrankaComponent::startHook(){
   std::cout << "Franka_panda_driver started !" <<std::endl;
   panda->control(
   [&  ](const franka::RobotState& state, franka::Duration period) -> franka::JointVelocities {
-    
+
     for(unsigned int i=0;i<NR_JOINT;++i) {
-      m_joint_states.position[i]   = state.q[i];
-      m_joint_states.velocity[i] = state.dq[i];
+      // m_joint_states.position[i]   = state.q[i];
+      // m_joint_states.velocity[i] = state.dq[i];
+      temporary_actual_pos[i] = state.q[i];
     }
-    
-    port_joint_state.write(m_joint_states);
-    
+    for(unsigned int i=0;i<6;++i) {
+      temporary_actual_wrench[i] = state.K_F_ext_hat_K[i];
+    }
+
+    sensor_joint_angles.write(temporary_actual_pos);
+    tool_external_wrench.write(temporary_actual_wrench);
+
     franka::JointVelocities velocities = {{0.0,0.0,0.0,0.0,0.0,0.0,0.0}};
     return velocities;
   });
@@ -103,6 +91,18 @@ void FrankaComponent::stopHook() {
 
 void FrankaComponent::cleanupHook() {
   std::cout << "Franka_panda_driver cleaning up !" <<std::endl;
+}
+
+void FrankaComponent::start_sending_setpoints(){
+  //TODO: fill
+}
+
+std::vector<double> FrankaComponent::get_joint_angles(){
+  temporary_robot_state = panda->readOnce();
+  for(unsigned int i=0;i<NR_JOINT;++i) {
+    temporary_actual_pos[i] = temporary_robot_state.q[i];
+  }
+  return temporary_actual_pos;
 }
 
 ORO_CREATE_COMPONENT(FrankaComponent)
--- src/franka_panda_driver-component.hpp
+++ src/franka_panda_driver-component.hpp
@@ -27,39 +27,61 @@ class FrankaComponent : public RTT::TaskContext{
     void updateHook();
     void stopHook();
     void cleanupHook();
+    void start_sending_setpoints();
+    std::vector<double> get_joint_angles();
+
   private:
     PandaPtr panda;
-    
-    // Internal, mem alloc
-    motion_control_msgs::JointPositions  m_joint_pos_command;
-    motion_control_msgs::JointVelocities m_joint_vel_command;
-    motion_control_msgs::JointEfforts    m_joint_effort_command;
-    motion_control_msgs::JointEfforts    m_t_ext;
-    sensor_msgs::JointState              m_joint_states;
-    vector<double>                       m_q_des;
-    vector<double>                       m_q_actual;
-    vector<double>                       m_t_actual;
-    vector<double>                       m_qdot_actual;
+    bool sending_setpoints;
 
-    
+
+    // Internal, mem alloc
+    // motion_control_msgs::JointPositions  m_joint_pos_command;
+    // motion_control_msgs::JointVelocities m_joint_vel_command;
+    // motion_control_msgs::JointEfforts    m_joint_effort_command;
+    // motion_control_msgs::JointEfforts    m_t_ext;
+    // sensor_msgs::JointState              m_joint_states;
+    // vector<double>                       m_q_des;
+    // vector<double>                       m_q_actual;
+    // vector<double>                       m_t_actual;
+    // vector<double>                       m_qdot_actual;
+
+
+
     // Properties
-    std::string p_baseframe;
-    std::string p_ip_address;
-    
-    // Port Interface
-    InputPort<string>                               port_ein;
-    InputPort<vector<double> >                      port_qdes;
-    InputPort<motion_control_msgs::JointPositions>  port_joint_pos_command;
-    InputPort<motion_control_msgs::JointVelocities> port_joint_vel_command;
-    InputPort<motion_control_msgs::JointEfforts>    port_joint_effort_command;
+    // std::string p_baseframe;
+    // std::string p_ip_address;
     //
-    OutputPort<string>                              port_eout;
-    OutputPort<vector<double> >                     port_q_actual;
-    OutputPort<vector<double> >                     port_t_actual;
-    OutputPort<vector<double> >                     port_qdot_actual;
-    OutputPort<motion_control_msgs::JointPositions> port_joint_pos_msr;
-    OutputPort<motion_control_msgs::JointEfforts>   port_joint_ext_jnt;
-    OutputPort<sensor_msgs::JointState>             port_joint_state;
+    // // Port Interface
+    // InputPort<string>                               port_ein;
+    // InputPort<vector<double> >                      port_qdes;
+    // InputPort<motion_control_msgs::JointPositions>  port_joint_pos_command;
+    // InputPort<motion_control_msgs::JointVelocities> port_joint_vel_command;
+    // InputPort<motion_control_msgs::JointEfforts>    port_joint_effort_command;
+    // //
+    // OutputPort<string>                              port_eout;
+    // OutputPort<vector<double> >                     port_q_actual;
+    // OutputPort<vector<double> >                     port_t_actual;
+    // OutputPort<vector<double> >                     port_qdot_actual;
+    // OutputPort<motion_control_msgs::JointPositions> port_joint_pos_msr;
+    // OutputPort<motion_control_msgs::JointEfforts>   port_joint_ext_jnt;
+    // OutputPort<sensor_msgs::JointState>             port_joint_state;
+
+    // Internal, mem alloc
+    std::vector<double>                       temporary_desired_vel;
+    std::vector<double>                       temporary_actual_pos;
+    std::vector<double>                       temporary_actual_wrench;
+    franka::RobotState                        temporary_robot_state;
+
+
+    // Properties
+    std::string p_ip_address;
+
+    // Port Interface
+    RTT::InputPort<std::vector<double>>      control_joint_velocities;
+    RTT::OutputPort<std::vector<double>>     sensor_joint_angles;
+    RTT::OutputPort<std::vector<double>>     tool_external_wrench;
+
 
 };
 #endif
